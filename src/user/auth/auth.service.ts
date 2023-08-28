973786c4d183d919f7b7d0c4b9b69b9e05be774e
hkonate <konate.tronca@gmail.com>
2023-08-28 20:29:43 +0200

    add signup service

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
+export interface SignupParams {
+  firstname: string;
+  lastname: string;
+  pseudo: string;
+  phone?: string;
+  email: string;
+  password: string;
+  picture?: string;
+}
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Post()
+  async signup({
+    firstname,
+    lastname,
+    pseudo,
+    phone,
+    email,
+    password,
+    picture,
+  }: SignupParams) {
+    const userExist = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!userExist) {
+      throw new ConflictException();
+    }
+    const pseudoExist = await this.prismaService.user.findUnique({
+      where: {
+        pseudo,
+      },
+    });
+    if (!pseudoExist) {
+      throw new ConflictException();
+    }
+    const user = await this.prismaService.user.create({
+      data: {
+        firstname,
+        lastname,
+        pseudo,
+        phone,
+        email,
+        password,
+        picture,
+      },
+    });
+    console.log(user);
+  }
+}
