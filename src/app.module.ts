7cd946c6ef496a98df2fe00936c7785079a1c359
hkonate <konate.tronca@gmail.com>
2023-08-28 19:52:46 +0200

    adding module service controller for chat event user/auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { EventModule } from './event/event.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, EventModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
