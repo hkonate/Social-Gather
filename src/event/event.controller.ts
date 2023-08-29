3a70a517905bd2bcc158638c9e85244ab8382b0a
hkonate <konate.tronca@gmail.com>
2023-08-30 00:05:54 +0200

    add create event endpoint

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Put, Body } from '@nestjs/common';
+import { CreateEventDTO } from './dtos/event.dtos';
+import { EventService } from './event.service';
 
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(private readonly eventService: EventService) {}
+  @Post()
+  createEvent(@Body() body: CreateEventDTO) {
+    return this.eventService.createEvent(body);
+  }
+}
