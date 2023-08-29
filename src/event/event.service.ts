22a7a3bd20c02ff75903366c648ed3a7cda9499a
hkonate <konate.tronca@gmail.com>
2023-08-29 23:51:03 +0200

    add inclusion enum to event model and others attribut to user model

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
+interface CreateEventParams {
+  title: string;
+  description: string;
+  schedule: string;
+}
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(private readonly prismaService: PrismaService) {}
+  //Get all events
+
+  //Get one event
+
+  //Create an event
+  async createEvent() {}
+  //Join/Unjoin an event
+
+  //Update an event
+
+  //Delete an event
+}
