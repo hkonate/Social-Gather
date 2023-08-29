52319be680c8a5ca2fb1484d0a0cce23d02709a8
hkonate <konate.tronca@gmail.com>
2023-08-29 23:58:31 +0200

    add createEvent service

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InclusionType } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 interface CreateEventParams {
   title: string;
   description: string;
   schedule: string;
+  inclusive: InclusionType[];
 }
 @Injectable()
 export class EventService {
@@ -14,7 +16,17 @@ export class EventService {
   //Get one event
 
   //Create an event
-  async createEvent() {}
+  createEvent({ title, description, schedule, inclusive }: CreateEventParams) {
+    return this.prismaService.event.create({
+      data: {
+        title,
+        description,
+        schedule,
+        inclusive,
+        creatorId: 'fc2f8777-27a1-4248-9ce5-4f963d81d203',
+      },
+    });
+  }
   //Join/Unjoin an event
 
   //Update an event
