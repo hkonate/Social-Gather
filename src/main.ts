0c70575d6fe439522d9872e47929fbcb75b15b17
hkonate <konate.tronca@gmail.com>
2023-10-24 15:10:48 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
